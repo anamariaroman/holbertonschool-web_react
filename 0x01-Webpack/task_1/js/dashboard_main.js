ad3dd8254a8e7f5705a0db0c149f430708e8b0bc
anamariaroman <anamariaromanvalencia@gmail.com>
2022-05-23 05:50:29 +0700

    changes

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
 import $ from 'jquery';
-import _ from 'lodash';
+const _ = require('lodash');
+
+$.when( $.ready ).then(function() {
+
+let counter = 0;
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append($('<p>').attr('id', 'count'));
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-
-function updateCounter() { count++; }
-
-$('button').click(_.debounce(() => {
-  updateCounter();
-  $('#count').text(`${count} clicks on the button`);
-}));
+$('button').on('click', _.debounce(updateCounter, 500));
+});
